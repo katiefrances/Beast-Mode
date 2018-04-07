90d7b071906ff27544f64720de922466cc7fa68c
Katie Frances <katiebeeisdead@gmail.com>
2018-04-07 16:30:32 +1000

    Added hashes

---
+++ Hashes/beast_mode.rb
@@ -0,0 +1,27 @@
+# Ruby Hashes Beast Mode ++
+
+
+# Beast Mode ++
+# Create a ruby application that requests the name of a classmate, and displays their birthday and your birthday. Use at least one Hash and one Array in the solution.
+# Bonus:
+# 1. Display the number of days between your birthdays.
+# 1. Create a method to display their star sign based on their birthday.
+# 2. Someone's probably already coded this date to star sign functionality - replace your method with a gem
+
+my_birthday = 23091985
+
+# Ask user for their birthday & introduce myself
+puts "What is your name?"
+name = gets.chomp
+puts "Hello #{name}, my name is Katie"
+
+# Ask user for birthday & tell them mine
+puts "When's your birthday, buddy?"
+birthday = gets.chomp
+puts "So your, birthday is #{birthday}, huh? Interesting, my birthday is #{my_birthday}"
+
+
+# my birthday is this many days away from yours
+
+# one hash and one array
+
